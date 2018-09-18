feda6eed12a62d14d30cbf2c734aed6af140e864
Suzy Montalvo <35845501+SuzyMontalvo@users.noreply.github.com>
2018-09-17 17:09:43 -0700

    Add files via upload

---
+++ ASCIIArt.java
@@ -0,0 +1,45 @@
+package Kitty;
+
+public class ASCIIArt {
+	public static void main (String[] args) {
+		
+		honeyComb();
+		rightBee();
+		honeyComb();
+		leftBee();
+		honeyComb();
+		rightBee();
+		honeyComb();
+		leftBee();
+		honeyComb();
+		rightBee();
+		honeyComb();
+		leftBee();
+		
+
+		
+	}
+
+	public static void honeyComb() {
+		System.out.println(" __  __  __  __  __  __  __  __");
+		System.out.println("/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\");
+		System.out.println("\\__/\\__/\\__/\\__/\\__/\\__/\\__/\\__/");
+		
+	}
+	public static void rightBee() {
+		System.out.println(" __  __  __  __  __  __   \"Bzzz\" ");
+		System.out.println("/  \\/  \\/  \\/  \\/  \\/  \\   \\  / ");
+		System.out.println("\\__/\\__/\\__/\\__/\\__/\\__/  _('') ");
+		System.out.println("/  \\/  \\/  \\/  \\/  \\/  \\ (_\\  /_) ");
+		System.out.println("\\__/\\__/\\__/\\__/\\__/\\__/   (==)");
+		System.out.println("                           /  \\");
+	}
+	public static void leftBee() {
+		System.out.println("  \"Bzzz\"   __  __  __  __  __  __");
+		System.out.println("   \\  /   /  \\/  \\/  \\/  \\/  \\/  \\");
+		System.out.println("  _('')_  \\__/\\__/\\__/\\__/\\__/\\__/");
+		System.out.println(" (_\\  /_) /  \\/  \\/  \\/  \\/  \\/  \\");
+		System.out.println("   (==)   \\__/\\__/\\__/\\__/\\__/\\__/");
+		System.out.println("   /  \\ ");
+	}
+}
